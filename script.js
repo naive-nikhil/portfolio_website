67de32e35e79bf1cf75a2935cfbbd424e55cd28a
Nikhil Patel <68939870+naive-nikhil@users.noreply.github.com>
2024-11-08 11:14:11 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const navBtns = document.querySelectorAll("li");
+
+const allSections = {
+  Education: document.querySelector(".educations"),
+  Experience: document.querySelector(".experiences"),
+  Skills: document.querySelector(".skills"),
+  Achievements: document.querySelector(".achievements"),
+};
+
+function activeSection(btn) {
+  Object.values(allSections).forEach((section) =>
+    section.classList.remove("active")
+  );
+  navBtns.forEach((navBtn) => navBtn.classList.remove("active"));
+
+  allSections[btn.dataset.section].classList.add("active");
+  btn.classList.add("active");
+}
+
+for (let btn of navBtns) {
+  btn.addEventListener("click", function (e) {
+    e.preventDefault();
+    activeSection(btn);
+  });
+}
